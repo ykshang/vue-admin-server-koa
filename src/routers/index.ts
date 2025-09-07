c4f46df3fda68961313a2d857938b8f8acf46984
kangkang shang <535634238@qq.com>
2025-09-07 21:14:09 +0800

    docs(routers): 添加路由合并注释
    在路由配置文件添加注释说明路由合并功能，提高代码可读性

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import Router from '@koa/router';
+
 import baseRouter from './base.router'
 import dictionaryRouter from './dictionary.routers'
 
 const router = new Router();
 
+// 合并所有路由
 router.use(baseRouter.routes(), baseRouter.allowedMethods());
 router.use(dictionaryRouter.routes(), dictionaryRouter.allowedMethods());
 

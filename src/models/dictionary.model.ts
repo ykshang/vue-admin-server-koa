366abca7f67afddfb1f030d032bc9a323c91509a
kangkang shang <535634238@qq.com>
2025-09-08 18:19:10 +0800

    feat(模型): 添加字典数据模型定义
    新增字典数据模型文件，包含字典键、名称、描述等字段定义及时间戳处理

---
+++ src/models/dictionary.model.ts
@@ -0,0 +1,61 @@
+import mongoose from "@/config/database"; // 复用连接
+import { extFieldDocument } from "@/types/base";
+import { Schema, model, Document } from "mongoose";
+
+// 定义用户接口
+export interface DictionaryInterface extends extFieldDocument, Document {
+  dictionaryKey: string;
+  dictionaryName: string;
+  desc: string;
+  createdAt: Date;
+  updatedAt: Date;
+  // 虚拟字段：格式化创建时间
+  formattedCreatedAt: string;
+  formattedUpdatedAt: string;
+}
+
+// 定义用户模型
+const schemaDefinition = new mongoose.Schema({
+  // 字典关键词
+  dictionaryKey: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  // 字典名称
+  dictionaryName: {
+    type: String,
+    required: true,
+    unique: false,
+    trim: true,
+    minlength: 5,
+    maxlength: 100
+  },
+  // 描述
+  description: {
+    type: String,
+    default: '',
+    required: false,
+    unique: false,
+    trim: true,
+    minlength: 0,
+    maxlength: 200
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (v: Date) => v.getTime() // 读取时转为毫秒数
+  }, // 创建时间
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    get: (v: Date) => v.getTime() // 读取时转为毫秒数
+  }, // 更新时间
+});
+schemaDefinition.set('toJSON', { getters: true });
+
+// 导出模型
+export default mongoose.model('dictionary', schemaDefinition, 'dictionary')

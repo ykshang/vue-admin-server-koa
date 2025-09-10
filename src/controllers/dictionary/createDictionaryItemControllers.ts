82db20913e60b5533f6fc9d3ac070cf32e716a76
kangkang shang <535634238@qq.com>
2025-09-11 00:38:28 +0800

    feat(字典): 添加请求体日志输出
    在创建字典项控制器中添加请求体日志输出，便于调试和追踪请求数据

--- src/controllers/dictionary/createDictionaryItemControllers.ts
+++ src/controllers/dictionary/createDictionaryItemControllers.ts
@@ -1,6 +1,8 @@
 import { Context } from "koa";
 
 export default function createDictionaryItem(ctx: Context) {
+  let request = ctx.request.body;
+  console.log('createDictionaryItem：', request);
   return ctx.body = {
     success: true,
     message: '获取字典列表成功',

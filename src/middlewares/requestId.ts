d1dd68705f8c4cdc11492357f2aa8bd6dc940349
kangkang shang <535634238@qq.com>
2025-09-11 01:11:14 +0800

    refactor(middlewares): 将请求ID从ctx.state移动到ctx.requestId
    简化请求ID的存储位置，直接从ctx访问而不是通过ctx.state，使代码更直观

--- src/middlewares/requestId.ts
+++ src/middlewares/requestId.ts
@@ -2,7 +2,7 @@ import { Context } from 'koa';
 import { v4 } from 'uuid';
 
 export default async function(ctx: Context, next: () => Promise<any>) {
-  ctx.state.reqId = v4(); // 注入到state
-  ctx.set('X-Request-Id', ctx.state.reqId);
+  ctx.requestId = v4(); // 注入到state
+  ctx.set('X-Request-Id', ctx.requestId);
   await next();
 }

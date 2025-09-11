ea815ccca6d12c9cd4ef323bf306dbdacae5de32
kangkang shang <535634238@qq.com>
2025-09-12 01:29:02 +0800

    refactor(middleware): 在日志中间件中添加文件名标识
    添加文件名变量到日志输出中，便于追踪日志来源

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,13 +2,14 @@ import { Context } from "vm";
 
 import logger from "@/utils/logger"; // 导入我们刚创建的logger
 
+let fileName = '[middleware/logger.ts]'
 // 创建一个自定义的日志中间件
 export default async function (ctx: Context, next: () => Promise<any>) {
   const start = Date.now(); // 记录开始时间
 
   // 请求开始时日志（体现位置：进入中间件）
   // ctx.state.reqId 是 koa-requestid 注入的
-  logger.info(`Request started: ${ctx.method} ${ctx.url}`,ctx.requestId);
+  logger.info(`Request started: ${ctx.method} ${ctx.url}`, fileName, ctx.requestId);
 
   // 等待后续中间件和执行链完成
   await next();
@@ -17,5 +18,5 @@ export default async function (ctx: Context, next: () => Promise<any>) {
   // 请求结束时日志（体现位置：离开中间件）
   // 状态码和耗时是关键信息
   logger.info(
-    `Request ended: ${ctx.method} ${ctx.url} - Status: ${ctx.status} - ${ms}ms`, ctx.requestId);
+    `Request ended: ${ctx.method} ${ctx.url} - Status: ${ctx.status} - ${ms}ms`, fileName, ctx.requestId);
 }

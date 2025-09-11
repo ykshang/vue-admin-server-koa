c2488b6d18ca188a82ccff3993a2dc794a2e2f1e
kangkang shang <535634238@qq.com>
2025-09-11 21:57:26 +0800

    refactor(字典服务): 优化日志记录参数避免重复文件名
    移除日志调用中重复的文件名参数，使日志输出更简洁

--- src/services/dictionary/createDictionaryItemService.ts
+++ src/services/dictionary/createDictionaryItemService.ts
@@ -2,9 +2,10 @@ import DictionaryItemModel from "@/models/dictionaryItem.model";
 import logger from "@/utils/logger"; // 导入我们刚创建的logger
 
 let fileName = '[Service] [createDictionaryItemService.ts]';
+
 export default async function createDictionaryItemService(request:any, CTX_REQ_ID?: string) {
-  logger.debug(fileName, '请求参数：', request, fileName, CTX_REQ_ID);
+  logger.debug('请求参数：', request, fileName, CTX_REQ_ID);
   let result = await new DictionaryItemModel(request).save();
-  logger.debug(fileName, '结果：', result, fileName, CTX_REQ_ID);
+  logger.debug('结果：', result, fileName, CTX_REQ_ID);
   return result;
 }

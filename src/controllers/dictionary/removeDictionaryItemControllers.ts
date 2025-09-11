d9f668dbd8a14292c803725de6e6e40a544b0d89
kangkang shang <535634238@qq.com>
2025-09-11 22:30:14 +0800

    feat(字典): 实现删除字典项功能
    - 在removeDictionaryItemService中实现删除字典项的逻辑
    - 在removeDictionaryItemControllers中添加删除结果的判断和响应
    - 添加日志记录功能以便调试

--- src/controllers/dictionary/removeDictionaryItemControllers.ts
+++ src/controllers/dictionary/removeDictionaryItemControllers.ts
@@ -1,12 +1,28 @@
 import { Context } from "koa";
+import logger from '@/utils/logger';
+import DictionaryService from '@/services/dictionary';
 
-// let fileName = '[Controller] [removeDictionaryItemControllers.ts]';
+let fileName = '[removeDictionaryItemControllers.ts]';
 
-export default function removeDictionaryItem(ctx: Context) {
+export default async function removeDictionaryItem(ctx: Context) {
   let CTX_REQ_ID = ctx.requestId;
   let request = ctx.request.body;
-  return ctx.body = {
-    success: true,
-    message: '获取字典列表成功',
+  logger.debug('请求参数：', request, fileName, CTX_REQ_ID);
+  let result = await DictionaryService.removeDictionaryItem(request, CTX_REQ_ID);
+  logger.debug('删除字典项结果：', result, fileName, CTX_REQ_ID);
+  if (result.deletedCount == 1) {
+    ctx.body = {
+      code: 200,
+      success: true,
+      message: '删除字典项成功',
+      data: result,
+    }
+  } else {
+    ctx.body = {
+      code: 500,
+      success: false,
+      message: '删除字典项失败：未知原因',
+      data: result,
+    }
   }
 }
--- src/services/dictionary/removeDictionaryItemService.ts
+++ src/services/dictionary/removeDictionaryItemService.ts
@@ -1,9 +1,12 @@
-// let fileName = '[Service] [removeDictionaryItemService.ts]';
+import DictionaryItemModel from '@/models/dictionaryItem.model';
+import logger from '@/utils/logger';
 
-export default async function removeDictionaryItemService(request: any) {
-  console.log(request)
-  return {
-    success: true,
-    message: '获取字典列表成功',
-  }
+let fileName = '[Service] [removeDictionaryItemService.ts]';
+
+export default async function removeDictionaryItemService(request: any, CTX_REQ_ID?: string) {
+  logger.debug('删除字典项请求参数：', request, fileName, CTX_REQ_ID);
+  let result = await DictionaryItemModel.deleteOne({
+    _id: request._id,
+  })
+  return result;
 }

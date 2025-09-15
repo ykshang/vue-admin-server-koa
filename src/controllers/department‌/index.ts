572b0444e90b4b86f69317ef93ab1057c7b88431
kangkang shang <535634238@qq.com>
2025-09-15 17:55:06 +0800

    feat(部门控制器): 添加获取部门列表功能
    新增 getDepartmentList 控制器并导出，用于获取部门列表数据

--- src/controllers/department‌/index.ts
+++ src/controllers/department‌/index.ts
@@ -1,5 +1,7 @@
 import createDepartment‌ from "./createDepartment‌Controllers";
+import getDepartment‌List‌ from "./getDepartment‌List‌Controllers";
 
 export default {
   createDepartment‌,
+  getDepartment‌List‌,
 }

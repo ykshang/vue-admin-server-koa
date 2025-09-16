eb27f470772a64f52142dc1d7ec18970d0bedbce
kangkang shang <535634238@qq.com>
2025-09-16 23:57:19 +0800

    docs(logger): 为日志工具函数添加JSDoc注释
    为transformParames函数和各个日志级别函数添加详细的JSDoc注释，说明函数用途和参数含义

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,10 @@
 import logger from "@/config/logger";
-
+/**
+ * 转换日志参数
+ * @description 转换日志参数，将参数中的reqId、fileName、json对象、正则表达式转换为字符串
+ * @param args 日志参数
+ * @returns 转换后的日志参数
+ */
 function transformParames(args: any[]) {
   let reqId = args.pop();
   let fileName = args.pop();
@@ -27,23 +32,46 @@ function transformParames(args: any[]) {
     },
   };
 }
+/**
+ * error日志
+ * @description 记录error日志
+ * @param args 日志参数
+ */
 function error(...args: any[]) {
   let { message, context } = transformParames(args);
   logger.error(message, context);
 }
+/**
+ * info日志
+ * @description 记录info日志
+ * @param args 日志参数
+ */
 function info(...args: any[]) {
   let { message, context } = transformParames(args);
   logger.info(message, context);
 }
+/**
+ * debug日志
+ * @description 记录debug日志
+ * @param args 日志参数
+ */
 function debug(...args: any[]) {
   let { message, context } = transformParames(args);
   logger.debug(message, context);
 }
+/**
+ * warn日志
+ * @description 记录warn日志
+ * @param args 日志参数
+ */
 function warn(...args: any[]) {
   let { message, context } = transformParames(args);
   logger.warn(message, context);
 }
-
+/**
+ * 日志对象
+ * @description 日志对象，输出error、info、debug、warn等级别的日志
+ */
 export default {
   info,
   error,

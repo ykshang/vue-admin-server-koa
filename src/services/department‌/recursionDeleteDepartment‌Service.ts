469a6ca4284d72a0d31cbf3afe8eab3320900d84
kangkang shang <535634238@qq.com>
2025-09-16 23:47:26 +0800

    refactor(department): 优化递归删除部门的代码逻辑
    重构部门删除服务中的部门代码处理逻辑，使用更简洁的方式生成正则表达式匹配模式。通过直接截断末尾的'00'而不是拆分再过滤，提高了代码的可读性和性能。

--- src/services/department‌/recursionDeleteDepartment‌Service.ts
+++ src/services/department‌/recursionDeleteDepartment‌Service.ts
@@ -4,14 +4,13 @@ import logger from "@/utils/logger"; // 导入我们刚创建的logger
 let fileName = '[Service] [recursionDeleteDepartment‌Service.ts]';
 export default async function deleteDepartment‌Service(departmentCode: string, CTX_REQ_ID?: string) {
   logger.debug("要删除的部门：", departmentCode, fileName, CTX_REQ_ID);
-  const codeList = departmentCode.split('');
-  let tempCode = []
-  while (codeList.length > 0) {
-    tempCode.push(codeList.splice(0,2).join(""));
+  // 组装部门前缀
+  let code = departmentCode
+  while (code.endsWith('00')) {
+    code = code.slice(0, -2)
   }
-  tempCode = tempCode.filter(item => item !== '00');
   let params = {
-    departmentCode: { $regex: new RegExp(`^${tempCode.join("")}`) } 
+    departmentCode: { $regex: new RegExp(`^${code}`) } 
   }
   let result = await Department‌Model.deleteMany(params);
   logger.debug("部门的递归删除结果：", result, fileName, CTX_REQ_ID);
